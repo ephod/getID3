a59da190d2fc4ce14116fb91d4f44d7e7a254c02
Michael <michael.johnson.lucas@gmail.com>
2018-02-20 21:17:34 +0100

    Add Test stub

---
+++ tests/GetId3Test.php
@@ -0,0 +1,115 @@
+<?php
+declare(strict_types=1);
+
+namespace GetID3;
+
+use PHPUnit\Framework\TestCase;
+
+class GetId3Test extends TestCase
+{
+
+    public function testFread_buffer_size()
+    {
+
+    }
+
+    public function testGetHashdata()
+    {
+
+    }
+
+    public function testInclude_module()
+    {
+
+    }
+
+    public function testSetOption()
+    {
+
+    }
+
+    public function testOpenfile()
+    {
+
+    }
+
+    public function testGetFileFormatArray()
+    {
+
+    }
+
+    public function testCalculateCompressionRatioAudio()
+    {
+
+    }
+
+    public function testProcessAudioStreams()
+    {
+
+    }
+
+    public function testError()
+    {
+
+    }
+
+    public function testCalculateCompressionRatioVideo()
+    {
+
+    }
+
+    public function testVersion()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testCharConvert()
+    {
+
+    }
+
+    public function testIs_writable()
+    {
+
+    }
+
+    public function testHandleAllTags()
+    {
+
+    }
+
+    public function testChannelsBitratePlaytimeCalculations()
+    {
+
+    }
+
+    public function testGetFileFormat()
+    {
+
+    }
+
+    public function testAnalyze()
+    {
+
+    }
+
+    public function testWarning()
+    {
+
+    }
+
+    public function testCalculateReplayGain()
+    {
+
+    }
+
+    public function testGetid3_tempnam()
+    {
+
+    }
+}
