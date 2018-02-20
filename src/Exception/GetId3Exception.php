0a63d47a0578a6cd859e34d3639910d4ee1d1389
Michael <michael.johnson.lucas@gmail.com>
2018-02-20 19:33:12 +0100

    Add main class, abstract class and general exception class
    Separation of concerns based on getid3.php

---
+++ src/Exception/GetId3Exception.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetID3\Exception;
+
+use Exception;
+
+/**
+ * Class GetId3Exception
+ *
+ * @package GetID3\Exception
+ */
+class GetId3Exception extends Exception
+{
+
+}
