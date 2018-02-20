96468017cef14108448fb5498eaf61585669dee5
Michael <michael.johnson.lucas@gmail.com>
2018-02-20 19:15:22 +0100

    Move files
    Organize files. Move them to their correspondent folder, according to the best practices of PHP Composer packages

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>getID3 demos</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<main>
+    <section>
+        <div>
+            <p>In this directory are a number of examples of how to use <a
+                        href="http://www.getid3.org/">getID3()</a>.</p>
+            <p>If you don't know what to run, take a look at <a
+                        href="demos/browse.php">browse.php</a></p>
+        </div>
+    </section>
+    <section>
+        <div>
+            <h3>Other demos</h3>
+            <ol>
+                <ul><a href="demos/audioinfo.class.php"></a>audioinfo.class.php</a></ul>
+                <ul><a href="demos/basic.php">basic.php</a></ul>
+                <ul><a href="demos/cache.dbm.php">cache.dbm.php</a></ul>
+                <ul><a href="demos/cache.mysql.php">cache.mysql.php</a></ul>
+                <ul><a href="demos/dirscan.php">dirscan.php</a></ul>
+                <ul><a href="demos/joinmp3.php">joinmp3.php</a></ul>
+                <ul><a href="demos/mimeonly.php">mimeonly.php</a></ul>
+                <ul><a href="demos/mp3header.php">mp3header.php</a></ul>
+                <ul><a href="demos/mysql.php">mysql.php</a></ul>
+                <ul><a href="demos/simple.php">simple.php</a></ul>
+                <ul><a href="demos/simple.write.php">simple.write.php</a></ul>
+                <ul><a href="demos/write.php">write.php</a></ul>
+                <ul><a href="demos/zip.php">zip.php</a></ul>
+            </ol>
+        </div>
+    </section>
+</main>
+</body>
+</html>
